f6374056ce55286bdf670a92be24187327f39fc4
Milt0n <cedric.lood@gmail.com>
2016-05-09 08:25:07 +0200

    beyond linearity

--- splines.R
+++ splines.R
@@ -26,9 +26,9 @@ lines(age.grid,pred2$fit,col="red",lwd=2)
 
 png("splines_natural.png")
 plot(age,wage,col="gray")
-lines(age.grid,pred$fit2,lwd=2)
-lines(age.grid,pred$fit2+2*pred$se,lty="dashed")
-lines(age.grid,pred$fit2-2*pred$se,lty="dashed")
+lines(age.grid,pred2$fit,lwd=2)
+lines(age.grid,pred2$fit+2*pred2$se,lty="dashed")
+lines(age.grid,pred2$fit-2*pred2$se,lty="dashed")
 dev.off()
 
 # smoothing splines
@@ -41,13 +41,16 @@ fit2$df
 lines(fit,col="red",lwd=2)
 lines(fit2,col="blue",lwd=2)
 legend("topright",legend=c("16 DF", "6.8 DF"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
+dev.off()
 
+# local regression
+png("splines_local_regression.png")
 plot(age,wage,xlim=agelims,cex=.5,col="darkgrey")
 title("Local regression")
-fit=loess(wage~age,span = .2,data=wage)
-fit2=loess(wage~age,span=.5,data=wage)
+fit=loess(wage~age,span = .2,data=Wage)
+fit2=loess(wage~age,span=.5,data=Wage)
 lines(age.grid,predict(fit,data.frame(age=age.grid)),col="red",lwd=2)
 lines(age.grid,predict(fit2,data.frame(age=age.grid)),col="blue",lwd=2)
 legend("topright",legend=c("16 DF", "6.8 DF"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
-
+dev.off()
 
